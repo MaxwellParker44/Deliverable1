06837d54e23c09a570f85d4f5554e07f2108efce
MaxwellParker44 <59374513+MaxwellParker44@users.noreply.github.com>
2019-12-30 16:23:32 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Max_s_Real_Deliverable_1
+{
+    class Program
+    {
+        static object Main(string[] args)
+        {
+            Console.WriteLine("What type of vacation do you want to take? Tropical, Musical, or Adventurous"[]);
+            {
+                Console.WriteLine("How many are in your group? 1-2, 3-4, 6+"[]);
+            }  
+            if 
+        }
+    }
+}
